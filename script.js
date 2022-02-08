d8249426279124cab480ef71020e5f6783b6625b
99058830 <99058830@mydavinci.nl>
2022-02-08 10:13:29 +0100

    added consolelog

--- script.js
+++ script.js
@@ -10,9 +10,11 @@ btn.onclick = function() {
     if (btn.innerHTML == 'Switch light on') {
         document.body.style.backgroundColor = 'yellow';
         btn.innerHTML = 'Switch light off';
+        console.log('Light on')
     } else if (btn.innerHTML == 'Switch light off') {
         document.body.style.backgroundColor = 'black';
         btn.innerHTML = 'Switch light on';
+        console.log('Light off')
     }
 };
 // schijf hier tussen je code
