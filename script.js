d37c755c0a61f04caf098cf14e9925c61cbbea2b
99058830 <99058830@mydavinci.nl>
2022-02-08 09:08:38 +0100

    added files

---
+++ script.js
@@ -0,0 +1,9 @@
+var btn = document.createElement('button');
+btn.style.margin = '10px';
+btn.innerHTML = '...';
+document.body.appendChild(btn);
+
+// schijf hier tussen je code
+
+
+// schijf hier tussen je code
