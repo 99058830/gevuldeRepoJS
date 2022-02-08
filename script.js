45a53e204316fded7410e31515a452e467a502b0
99058830 <99058830@mydavinci.nl>
2022-02-08 10:07:55 +0100

    fixed turn off and on

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 var btn = document.createElement('button');
 btn.style.margin = '10px';
-btn.innerHTML = '...';
+btn.innerHTML = 'Switch light on';
+document.body.style.backgroundColor = 'black';
 document.body.appendChild(btn);
 
 // schijf hier tussen je code
 
-
+btn.onclick = function() {
+    if (btn.innerHTML == 'Switch light on') {
+        document.body.style.backgroundColor = 'yellow';
+        btn.innerHTML = 'Switch light off';
+    } else if (btn.innerHTML == 'Switch light off') {
+        document.body.style.backgroundColor = 'black';
+        btn.innerHTML = 'Switch light on';
+    }
+};
 // schijf hier tussen je code
